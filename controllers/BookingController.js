658a6c57735c24648ee26e899c311c18702478d8
bogdan9876 <bogdankryveiko@gmail.com>
2025-03-23 22:08:20 +0200

    add babel

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -1,53 +1,10 @@
-class BookingController {
-    constructor(bookingService) {
-        this.bookingService = bookingService;
-    }
+const BaseController = require('./BaseController.js');
+const BookingService = require('../services/BookingService.js');
 
-    async create(req, res) {
-        try {
-            const result = await this.bookingService.create(req.body);
-            res.json(result);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
-    }
-
-    async getAll(req, res) {
-        try {
-            const result = await this.bookingService.getAll();
-            res.json(result);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
-    }
-
-    async getOne(req, res) {
-        try {
-            const result = await this.bookingService.getById(req.params.id);
-            if (!result) return res.status(404).json({ message: "Not found" });
-            res.json(result);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
-    }
-
-    async update(req, res) {
-        try {
-            const result = await this.bookingService.update(req.params.id, req.body);
-            res.json(result);
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
-    }
-
-    async delete(req, res) {
-        try {
-            await this.bookingService.delete(req.params.id);
-            res.json({ message: "Deleted successfully" });
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+class BookingController extends BaseController {
+    constructor() {
+        super(BookingService);
     }
 }
 
-module.exports = BookingController;
+module.exports = new BookingController;
--- routes/BookingRouter.js
+++ routes/BookingRouter.js
@@ -1,14 +1,9 @@
 const BaseRouter = require("./BaseRouter.js");
 const BookingController = require("../controllers/BookingController.js");
-const BookingService = require("../services/BookingService.js");
-const BookingRepository = require("../repositories/BookingRepository.js");
 
 class BookingRouter extends BaseRouter {
     constructor() {
-        const bookingRepository = new BookingRepository();
-        const bookingService = new BookingService(bookingRepository);
-        const bookingController = new BookingController(bookingService);
-        super(bookingController);
+        super(BookingController);
     }
 }
 
