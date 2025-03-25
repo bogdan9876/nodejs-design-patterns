66fee3ce08a93162db3dac3a0c1cff00ea87be1c
bogdan9876 <bogdankryveiko@gmail.com>
2025-03-25 11:49:30 +0200

    final data generator code

--- src/generateData.ts
+++ src/generateData.ts
@@ -4,52 +4,71 @@ function generateCSV(filePath: string) {
     const writeStream = fs.createWriteStream(filePath);
 
     writeStream.write(
-        'userId,name,email,roomId,number,type,price,checkInDate,checkOutDate,hotelId,hotelName,rating,comment\n'
+        'hotelChainId,hotelChainName,hotelId,hotelName,hotelAddress,hotelCity,hotelCountry,roomId,roomNumber,roomType,roomPrice,isAvailable,userId,userName,userEmail,bookingId,bookingNumber,bookingType,bookingPrice,checkInDate,checkOutDate,reviewId,rating,comment\n'
     );
 
-    const users = [];
-    const hotels = [];
-    const rooms = [];
+    let hotelChainId = 1;
+    let hotelId = 1;
+    let roomId = 1;
+    let userId = 1;
+    let bookingId = 1;
+    let reviewId = 1;
 
-    for (let i = 0; i < 1000; i++) {
-        const userId = i + 1;
-        users.push(userId);
-        const userName = `User_${userId}`;
-        const userEmail = `user${userId}@gmail.com`;
+    const hotelChains = ['Hilton', 'Marriott', 'Hyatt', 'Sheraton', 'InterContinental'];
 
-        const hotelId = Math.floor(Math.random() * 5) + 1;
-        hotels.push(hotelId);
-        const hotelName = `Hotel_${hotelId}`;
+    for (const chain of hotelChains) {
+        const hotelsInChain = Math.floor(Math.random() * 5) + 1;
 
-        const roomId = (i % 10) + 1;
-        rooms.push(roomId);
-        const roomNumber = 101 + (i % 20);
-        const roomType = i % 2 === 0 ? 'Single' : 'Double';
-        const roomPrice = (50 + (i % 50)).toFixed(2);
+        for (let h = 0; h < hotelsInChain; h++) {
+            const hotelName = `${chain} Hotel ${hotelId}`;
+            const hotelAddress = `Street ${hotelId}`;
+            const hotelCity = `City_${hotelId}`;
+            const hotelCountry = `Country_${hotelId}`;
 
-        const checkInDate = `2025-03-${(i % 30) + 1}`;
-        const checkOutDate = `2025-03-${(i % 30) + 10}`;
+            const roomsInHotel = Math.floor(Math.random() * 10) + 5;
 
-        const rating = Math.floor(Math.random() * 5) + 1;
-        const comment = i % 2 === 0 ? 'Great stay!' : 'Good experience';
+            for (let r = 0; r < roomsInHotel; r++) {
+                const roomNumber = 100 + roomId;
+                const roomType = roomId % 2 === 0 ? 'Single' : 'Double';
+                const roomPrice = (50 + (roomId % 50)).toFixed(2);
+                const isAvailable = Math.random() > 0.2;
 
-        const dataRow = [
-            userId,
-            userName,
-            userEmail,
-            roomId,
-            roomNumber,
-            roomType,
-            roomPrice,
-            checkInDate,
-            checkOutDate,
-            hotelId,
-            hotelName,
-            rating,
-            comment
-        ].join(',');
+                const usersCount = Math.floor(Math.random() * 3) + 1;
+                for (let u = 0; u < usersCount; u++) {
+                    const userName = `User_${userId}`;
+                    const userEmail = `user${userId}@gmail.com`;
 
-        writeStream.write(dataRow + '\n');
+                    const bookingNumber = bookingId;
+                    const bookingType = roomType;
+                    const bookingPrice = roomPrice;
+                    const checkInDate = `2025-03-${(bookingId % 28) + 1}`;
+                    const checkOutDate = `2025-03-${(bookingId % 28) + 3}`;
+
+                    const rating = Math.floor(Math.random() * 5) + 1;
+                    const comment = rating > 3 ? 'Great stay!' : 'Good experience';
+
+                    const dataRow = [
+                        hotelChainId, chain, hotelId, hotelName, hotelAddress, hotelCity, hotelCountry,
+                        roomId, roomNumber, roomType, roomPrice, isAvailable,
+                        userId, userName, userEmail,
+                        bookingId, bookingNumber, bookingType, bookingPrice, checkInDate, checkOutDate,
+                        reviewId, rating, comment
+                    ].join(',');
+
+                    writeStream.write(dataRow + '\n');
+
+                    userId++;
+                    bookingId++;
+                    reviewId++;
+                }
+
+                roomId++;
+            }
+
+            hotelId++;
+        }
+
+        hotelChainId++;
     }
 
     writeStream.end();
