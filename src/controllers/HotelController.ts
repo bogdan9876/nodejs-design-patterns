0b846937b0aed54f6886ae4f4e9d6c29e5870529
bogdan9876 <bogdankryveiko@gmail.com>
2025-04-03 11:58:55 +0300

    demo update method

--- src/controllers/HotelController.ts
+++ src/controllers/HotelController.ts
@@ -17,6 +17,19 @@ class HotelController extends BaseController<IHotelService> {
         }
     }
 
+    async editHotelPage(req: Request, res: Response): Promise<void> {
+        try {
+            const hotel = await this.service.getById(req.params.id);
+            if (!hotel) {
+                res.status(404).send("Hotel not found");
+                return;
+            }
+            res.render('edit_hotel', { hotel });
+        } catch (error) {
+            res.status(500).json({ message: (error as Error).message });
+        }
+    }
+
     async create(req: Request, res: Response): Promise<void> {
         try {
             const newHotel = await this.service.create(req.body);
@@ -30,7 +43,7 @@ class HotelController extends BaseController<IHotelService> {
         await this.handleRequest(req, res, async () => {
             try {
                 const hotels = await this.service.getAll();
-                res.render( 'index' ,{ hotels });
+                res.render('index', { hotels });
             } catch (error) {
                 res.status(500).json({ message: (error as Error).message });
             }
