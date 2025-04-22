fce394d2a37ab7f2d0c39617098780543f0bcfcb
bogdan9876 <bogdankryveiko@gmail.com>
2025-04-22 19:14:14 +0300

    fix missing async operator in kafka consumer

--- kafka_consumer/index.ts
+++ kafka_consumer/index.ts
@@ -1,16 +1,19 @@
 import { Kafka } from 'kafkajs';
 
-const kafka = new Kafka({ clientId: 'consumer', brokers: ['localhost:9092'] });
+async function startConsumer() {
+    const kafka = new Kafka({ clientId: 'consumer', brokers: ['localhost:9092'] });
+    const consumer = kafka.consumer({ groupId: 'test-group' });
 
-const consumer = kafka.consumer({ groupId: 'test-group' })
- 
-await consumer.connect()
-await consumer.subscribe({ topic: 'topic1', fromBeginning: true })
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'topic1', fromBeginning: true });
 
-await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-        console.log({
-            value: message.value!.toString(),
-        })
-    },
-})
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+                value: message.value!.toString(),
+            });
+        },
+    });
+}
+
+startConsumer()
